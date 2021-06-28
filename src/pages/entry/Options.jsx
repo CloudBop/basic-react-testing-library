82c063000f4258f51837c4c2d77f8d7c006f7252
Cloudbop <boppincolin@gmail.com>
2021-06-28 17:53:36 +0100

    updated jsx, test still failing

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
+import AlertBanner from '../common/AlertBanner';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false)
 
   // optionType is 'scoops' or 'toppings' - ts enum
   useEffect(() => {
@@ -14,9 +16,11 @@ export default function Options({ optionType }) {
       .then((response) => setItems(response.data))
       .catch((error) => {
         // TODO: handle error response
+        setError(true)
       });
   }, [optionType]);
 
+  if (error) return <AlertBanner variant='danger' />;
   // TODO: replace `null` with ToppingOption when available
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption
 
