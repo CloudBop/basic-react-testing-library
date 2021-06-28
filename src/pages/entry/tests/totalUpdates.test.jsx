cdcdbd79ba819b026b35dab70065cbad5972c772
Cloudbop <boppincolin@gmail.com>
2021-06-28 23:24:19 +0100

    +totalUpdates test setup + pseudocode

---
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Options from '../Options';
+test('update sccop subtotal when scoops change', () => {
+  // render(<Options optionType="scoops" />)
+
+  // init total to 0.00
+
+  // update vanilla scoops to 1
+
+  // update chocolate scoops to 2
+})
