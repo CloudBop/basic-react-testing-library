79735fb791b9b324bdf4fa23cb60ad17088108f9
Cloudbop <boppincolin@gmail.com>
2021-06-28 18:00:39 +0100

    passtest, should find two alerts on endpoint errors

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { rest } from 'msw';
 import OrderEntry from '../OrderEntry';
 import { server } from '../../../mocks/server.js'
@@ -13,7 +13,17 @@ test('handles errors for scoops and topping routes', async () => {
 
   render(<OrderEntry />)
 
-  const alerts = await screen.findAllByRole('alert', { name: "An unexpected error ocurred. Please try again later." })
+  // // will resolve on first response, but we're expecting from two async reqs
+  // const alerts = await screen.findAllByRole('alert',
+  //   // doesn't work... { name: "An unexpected error ocurred. Please try again later." }
+  // )
+  // expect(alerts).toHaveLength(2);
 
-  expect(alerts).toHaveLength(2);
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole('alert',
+      // doesn't work... { name: "An unexpected error ocurred. Please try again later." }
+    )
+    expect(alerts).toHaveLength(2);
+  })
+
 })
