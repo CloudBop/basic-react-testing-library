07ae3b6f3d5ee852f3d1caba51db59b98797fb40
Cloudbop <boppincolin@gmail.com>
2021-06-29 11:20:28 +0100

    +overrule testing-library

---
+++ src/testing-utilities/testing-library-utils.jsx
@@ -0,0 +1,12 @@
+//override this
+import { render } from "@testing-library/react";
+import { OrderDetailsProvider } from '../contexts/OrderDetails';
+
+const renderWithContext = (ui, options) => {
+  render(ui, { wrapper: OrderDetailsProvider, ...options })
+}
+
+// export this lib...
+export * from "@testing-library/react";
+// ... override the render method
+export { renderWithContext as render };
