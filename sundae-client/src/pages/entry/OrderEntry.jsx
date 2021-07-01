2fcdc4ccda49f37de4450af52cefc2d47eaa35ba
Cloudbop <boppincolin@gmail.com>
2021-07-01 17:57:19 +0100

    +setOrderPhase as props to each component phase

--- sundae-client/src/pages/entry/OrderEntry.jsx
+++ sundae-client/src/pages/entry/OrderEntry.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 import { useOrderDetails } from '../../contexts/OrderDetails'
 import Options from './Options'
-function OrderEntry() {
+function OrderEntry({ setOrderPhase }) {
   const [orderDetails] = useOrderDetails();
+
+  const orderDisabled = false;
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
+      <Button disabled={orderDisabled} onClick={() => setOrderPhase('review')}>
+        Order Sundae!
+      </Button>
     </div>
   )
 }
