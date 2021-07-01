9649f913fbcb73ce785dd5f8153ab2f88707c747
Cloudbop <boppincolin@gmail.com>
2021-07-01 17:54:40 +0100

    +app state for current phase of application

--- sundae-client/src/App.js
+++ sundae-client/src/App.js
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react';
+import { OrderDetailsProvider } from './contexts/OrderDetails';
 import Container from 'react-bootstrap/Container';
 import OrderEntry from './pages/entry/OrderEntry';
-import { OrderDetailsProvider } from './contexts/OrderDetails';
+import OrderConfirmation from './pages/confirmation/OrderConfirmation';
+import OrderSummary from './pages/summary/OrderSummary';
 
 export default function App() {
+  const [orderPhase, setOrderPhase] = useState('inProgress')
+
+  let Component = OrderEntry;
+
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break
+    case "review":
+      Component = OrderSummary;
+      break
+    case "completed":
+      Component = OrderConfirmation;
+      break
+    default:
+  }
+
   return (
     <OrderDetailsProvider>
       <Container>
-        <OrderEntry />
+        <Component setOrderPhase={setOrderPhase} />
       </Container>
     </OrderDetailsProvider>
   );
