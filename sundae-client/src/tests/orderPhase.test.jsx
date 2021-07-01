5c6927562970b39449f33a3e8677db2b9aa04013
Cloudbop <boppincolin@gmail.com>
2021-07-01 15:27:37 +0100

    order phase pseudocode

---
+++ sundae-client/src/tests/orderPhase.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+
+
+test("order phases for happy path (golden path/way)", () => {
+  //rnder app
+
+  //add ice cream scoops and toppings
+
+  //find and click order button
+
+  //check summary information based on order
+
+  //accept terms and conditions and click btn to confirm order 
+
+  //confirm order numer on confirmation page
+
+  //check scoops + toppins subtotals have been reset
+
+  //do we need to await anything to avoid test errors
+
+})
