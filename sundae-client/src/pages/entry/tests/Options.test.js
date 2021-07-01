9237c7a3fab0d48c03327a007f96fa04e6bd95f0
Cloudbop <boppincolin@gmail.com>
2021-07-01 22:34:07 +0100

    dont update total if scoops input is invalid

--- sundae-client/src/pages/entry/tests/Options.test.js
+++ sundae-client/src/pages/entry/tests/Options.test.js
@@ -1,3 +1,4 @@
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '../../../testing-utilities/testing-library-utils';
 import Options from '../Options';
 
@@ -25,4 +26,15 @@ test('displays image for each topping option from the server', async () => {
   //@ts-ignore
   const altText = toppingImages.map(element => element.alt);
   expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
+});
+test('don\'t update total if scoops input is invalid', async () => {
+  render(<Options optionType="scoops" />);
+  //find images
+  const vanillaInput = await screen.findByRole('spinbutton', { name: /vanilla/i });
+
+  userEvent.clear(vanillaInput)
+  userEvent.type(vanillaInput, "-1")
+
+  const scoopsSubtotal = screen.getByText('Scoops total: $0.00')
+  expect(scoopsSubtotal).toBeInTheDocument();
 });
